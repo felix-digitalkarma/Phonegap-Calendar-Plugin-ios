f12b2db910c4399832dd7eab588bdfc8ba2d75e6
Felix <felix@actv8.me>
2012-04-15 02:17:09 -0700

    updated project for cordova use

--- ExampleProject/CalendarPlugin/www/calendar/calendar.js
+++ ExampleProject/CalendarPlugin/www/calendar/calendar.js
@@ -1,10 +1,8 @@
 //
-// Cordova Calendar Plugin
+// PhoneGap Calendar Plugin
 // Author: Felix Montanez 
 // Created: 01-17-2012
 //
-// Contributors:
-// Michael Brooks
 
 
 function calendarPlugin()
@@ -18,7 +16,6 @@ calendarPlugin.prototype.createEvent = function(title,location,notes,startDate,e
     cordova.exec(null,null,"calendarPlugin","createEvent", [title,location,notes,startDate,endDate]);
 };
 
-// More methods will need to be added like fetch events, delete event, edit event
 
 calendarPlugin.install = function()
 {
