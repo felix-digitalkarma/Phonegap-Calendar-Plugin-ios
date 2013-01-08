22325d5eb9489242784e600481b3b9f4344f91ae
Felix Montanez <felix@felixmontanez.com>
2013-01-08 09:42:12 -0800

    adjusting plugin and adding new updated project

---
+++ CalendarPluginExample/CalendarPluginExample/Plugins/calendarPlugin.h
@@ -0,0 +1,42 @@
+//
+//  calendarPlugin.h
+//  Author: Felix Montanez
+//  Date: 01-17-2011
+//  Notes:
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+#import <EventKitUI/EventKitUI.h>
+#import <EventKit/EventKit.h>
+
+
+@interface calendarPlugin : CDVPlugin {
+    //NSArray *events;
+    
+    //future plan to have global type variables
+    
+    
+}
+
+
+@property (nonatomic, retain) EKEventStore* eventStore;
+
+- (void)initEventStoreWithCalendarCapabilities;
+
+-(NSArray*)findEKEventsWithTitle: (NSString *)title
+                        location: (NSString *)location
+                         message: (NSString *)message
+                       startDate: (NSDate *)startDate
+                         endDate: (NSDate *)endDate;
+
+// Calendar Instance methods
+- (void)createEvent:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+- (void)modifyEvent:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+- (void)findEvent:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+- (void)deleteEvent:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+@end
